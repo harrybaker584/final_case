413854be96e08fadf979a5677d2afd96270a393f
Harry Baker <harry.ichirin@gmail.com>
2024-11-22 14:43:39 +0000

    added customers intermediate model

--- models/intermediate/int_staff.sql
+++
@@ -1,9 +0,0 @@
---WITH staff_summary as(
-   -- SELECT staff_id,
-          -- sum(quantity) as total_products_sold,
-          -- sum(distinct())
-         --  sum(total_order_item_price) as total_amount_sold,
-          -- avg(percentage_discount) as average_staff_discount,
-
-
---)
