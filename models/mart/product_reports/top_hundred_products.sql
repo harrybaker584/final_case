53f0dd8959b3dc6cc3a2487fefc89942d9c62f6e
Harry Baker <harry.ichirin@gmail.com>
2024-11-25 14:30:51 +0000

    feat: added limit to products model to creat top_hundred_products

--- models/mart/product_reports/top_hundred_products.sql
+++ models/mart/product_reports/top_hundred_products.sql
@@ -3,6 +3,7 @@ SELECT product_name,
        sum(total_sales_amount) as total_sales_amount,
        avg(average_discount_on_product) as average_discount_on_product
 FROM {{ref("int_products")}}
+
 GROUP BY product_name
 ORDER BY total_sales_amount DESC
-LIMIT 100 
+LIMIT 100
