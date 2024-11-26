c1e600a97aeac10a9703e9504f054172c26fda8c
Harry Baker <harry.ichirin@gmail.com>
2024-11-26 13:58:24 +0000

    feat: correct custom tests

--- tests/is_positive.sql
+++ tests/is_positive.sql
@@ -1,7 +1,7 @@
-{% test is_positive(model,grouping_id,column_name)%}
-    {{grouping_id}},
-    sum({{column_name}}) as column_name_sum
-from {{ref("model")}}
-group by grouping_id
-having column_name_sum < 0
-{% endtest%}
+{% test is_positive(model, grouping_id, column_name) %}
+    select {{ grouping_id }}, 
+    sum({{ column_name }}) as column_name_sum
+    from {{ ref("model") }}
+    group by grouping_id
+    having column_name_sum < 0
+{% endtest %}
