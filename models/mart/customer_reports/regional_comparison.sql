3c2cfe64fc89e0a0e2d169367ced7f752171b554
Harry Baker <harry.ichirin@gmail.com>
2024-11-26 08:36:33 +0000

    feat: finished staff model to compare sales feat: added customer_schema and a readme ofr regional_comparisson as a test

--- models/mart/customer_reports/regional_comparison.sql
+++ models/mart/customer_reports/regional_comparison.sql
@@ -1,15 +1,17 @@
 select
             customer_state,
             sum(total_amount_spent) as total_region_spending,
-            avg(total_amount_spent) as average_regional_spending_per_customer,
+            avg(total_amount_spent) as average_regional_spending,
             sum(total_items_ordered) as total_region_items_ordered,
-            avg(total_items_ordered) as average_region_items_ordered_per_customer,
+            avg(total_items_ordered) as average_region_items_ordered,
             sum(total_distinct_products) as total_region_distinct_products_ordered,
             avg(total_distinct_products) as average_region_distinct_products_ordered,
             sum(total_orders) as total_region_orders,
             count(favourite_product_id) as favourite_product_id,
-        from {{ref('int_customers')}}
-        group by customer_state
+            p.product_name as favourite_product_name
+        from {{ref('int_customers')}} as c
+        left join {{ref("int_products")}} as p on c.favourite_product_id = p.product_id
+        group by customer_state, product_name
 
 
 
---
+++ models/mart/customer_reports/regional_comparisson.md
@@ -0,0 +1,14 @@
+{% docs int_sales_database_order %}
+
+This table contains information about sales across different regions (states)
+ - ** TOTAL REGION SPENDING :**  The total sum of customer spending in the region
+ - ** AVERAGE REGION SPENDING PER CUSTOMER**:  The avarge spending in the region 
+ - **  TOTAL REGION ITEMS ORDERED:** The total number of items ordered in the region
+ - ** AVERAGE REGION ITEMS ORDERED:** The average items ordered per customer in a region
+ - ** TOTAL REGION DISTINCT PRODUCTS ORDERED:** The total number of individual products ordered in a region
+ - ** AVERAGE REGION DISTINCT PRODUCTS ORDERED:** The average number of individual items ordered in a region
+ - ** TOTAL REGION ORDERS :** The total number of items ordered in a region
+ - ** FAVOURITE PRODUCT ID: ** The product_id for the most purchased product in the region
+ - ** FAVOURITE PRODUCT NAME:** The product name for the most purchased product in the region
+
+{% enddocs %}
