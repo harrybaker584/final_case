81f5440bb5d181a7935ac742857902faa67393fa
Harry Baker <harry.ichirin@gmail.com>
2024-11-26 14:48:32 +0000

    fixing test errors

--- tests/assert_discount_percent_is_percentage.sql
+++
@@ -1,6 +0,0 @@
-select
-    order_item_id,
-    sum(percentage_discount) as percentage_discount
-from {{ref('stg_order_items')}}
-group by order_item_id
-having percentage_discount<0 and percentage_discount>=100
